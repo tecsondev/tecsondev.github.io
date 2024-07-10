c87a2ca86105a5e9809251740c346c9e7cb00a68
tecsondev <tecsondev@gmail.com>
2024-07-10 16:38:45 +1000

    added contact js functionality reveal email

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ let minutes = date.getMinutes();
 let seconds = date.getSeconds();
 let time = `${hours}:${minutes}:${seconds}`;
 
-document.getElementById("time").innerHTML = time;
-setInterval(getTime, 1000);
+document.getElementById("time").innerHTML = time;};
+
+function emailReveal() {
+  document.getElementById("email").innerHTML = "tecsondev@gmail.com -";
 };
 
+function emailClear() {
+  document.getElementById("email").innerHTML = "email +";
+}
+
 getTime();
-
-
-
+setInterval(getTime, 1000);
